62f615db2c06fe7230ae11bd2ff6c02655b62fba
Keith Stone <kstone@whitepages.com>
2012-12-17 21:38:23 -0800

    Tweaks on resort update

--- app/models/ski_resort.rb
+++ app/models/ski_resort.rb
@@ -2,6 +2,7 @@ class SkiResort < ActiveRecord::Base
   has_many :snow_events
 
   def scrape_value!
+    logger.info "Starting scrape for #{name}"
     scrape = Nokogiri::HTML(open(scrape_url))
     scraped_value = scrape.at_css(scrape_selector).children.first.text
     match = scraped_value.match(Regexp.new(scrape_regex))
@@ -9,6 +10,8 @@ class SkiResort < ActiveRecord::Base
     if match && match[:value]
       self.value = match[:value] 
       save
+      logger.info "Saving scrape for #{name}"
     end
+    logger.info "Ending scrape for #{name}"
   end
 end
--- lib/tasks/ski_resorts.rake
+++ lib/tasks/ski_resorts.rake
@@ -2,7 +2,9 @@ namespace :resorts do
   desc "Updates ski_resort values based on scraping"
   task :update => :environment do
     SkiResort.find_each do |resort|
+      puts "Pushing #{resort.id}"
       RESORT_UPDATE_QUEUE.push(:resort_id => resort.id)
+      puts "Done pushing #{resort.id}"
     end
   end
 end
