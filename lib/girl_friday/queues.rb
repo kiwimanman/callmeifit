bfe42913d158c66929cc7117882fe8988319135c
Keith Stone <kstone@whitepages.com>
2012-12-27 20:11:45 -0800

    Fixing girl_friday.status

--- lib/girl_friday/queues.rb
+++ lib/girl_friday/queues.rb
@@ -1,18 +1,24 @@
 require 'connection_pool'
 
 redis_pool = ConnectionPool.new(:size => 5, :timeout => 5) {
-  Redis.new url: (ENV['MYREDIS_URL'] || 'redis://rediscloud@localhost:6379')
+  Redis.new url: ENV['MYREDIS_URL']
 }
 
 queue_defaults = {
   :size => ENV["WORKER"] == 'true' ? 1 : 0,
   :store => GirlFriday::Store::Redis,
-  :store_config => { :pool => redis_pool }
+  :store_config => { :pool => redis_pool },
 }
 
 ::PHONE_VERIFICATION_QUEUE = GirlFriday::WorkQueue.new(:phone_verification, queue_defaults) do |msg|
   Phone.send_verification(msg)
 end
+
+queue_defaults = {
+  :size => ENV["WORKER"] == 'true' ? 1 : 0,
+  :store => GirlFriday::Store::Redis,
+  :store_config => { :pool => redis_pool },
+}
 
 ::RESORT_UPDATE_QUEUE = GirlFriday::WorkQueue.new(:resort_update, queue_defaults) do |msg|
   Rails.logger.info("DEBUG: Updating resort #{msg[:resort_id]}")
