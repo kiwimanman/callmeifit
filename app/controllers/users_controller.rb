d28381fe6e4d984e0b37addb1fa9e813a872c39f
Keith Stone <kstone@whitepages.com>
2013-02-25 23:27:16 -0800

    Adding a users index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
-  before_filter :authorized_only
+  before_filter :authorized_only, except: :index
+
+  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'], only: :index
 
   def show
+    debugger
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.find(:all, :include => :facebook_keys)
   end
 
   protected
 
   def authorized_only
+    debugger
     redirect_to @logged_in_user unless @logged_in_user.id == params[:id].to_i
   end
 end
