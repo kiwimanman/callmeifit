5ab2a1f160fec754c40028c824adb04ea0106004
Keith Stone <kstone@whitepages.com>
2013-09-06 22:01:34 -0700

    User all is open

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :require_logged_in_user
 
-  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'], only: :index if Rails.env.production?
+  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'], only: :all if Rails.env.production?
 
   def all
     @users = User.all
