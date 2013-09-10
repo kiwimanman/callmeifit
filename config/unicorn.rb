c4b644571b4fba1912f81af901f1aa1ef2e830bf
Keith Stone <kstone@whitepages.com>
2013-09-09 20:11:53 -0700

    Lets kill bad connections to the DB

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,7 +15,10 @@ end
 after_fork do |server, worker|
 	# Replace with MongoDB or whatever
 	if defined?(ActiveRecord::Base)
-	  ActiveRecord::Base.establish_connection
+		config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL'] || 5
+	  ActiveRecord::Base.establish_connection(config)
 		Rails.logger.info('Connected to ActiveRecord')
 	end
 
