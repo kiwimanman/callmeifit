51bfc67c148a0f937efee3c8abb024565118304a
Keith Stone <kstone@whitepages.com>
2015-06-09 20:22:22 -0700

    Logging around Facebook login.

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,4 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           :secure_image_url => true
+  Rails.logger.info { "FACEBOOK_KEY=#{ENV['FACEBOOK_KEY']}" }
+  Rails.logger.info { "FACEBOOK_SECRET=#{ENV['FACEBOOK_SECRET']}" }
+
+  provider :facebook,
+           ENV['FACEBOOK_KEY'],
+           ENV['FACEBOOK_SECRET'],
+           secure_image_url: true,
+           client_options: {
+             site: 'https://graph.facebook.com/v2.0',
+             authorize_url: 'https://www.facebook.com/v2.0/dialog/oauth'
+           }
 end
